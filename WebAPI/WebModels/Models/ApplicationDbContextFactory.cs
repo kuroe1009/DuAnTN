d727b2d9f882ed2c3352d9bff79cc936f0e40ebc
hauhd05 <hauhd9525@gmail.com>
2025-06-08 12:24:02 +0700

    hau

---
+++ WebAPI/WebModels/Models/ApplicationDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WebModels.Models
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            // Lấy đường dẫn thư mục hiện tại
+            var basePath = Directory.GetCurrentDirectory();
+
+            // Load cấu hình appsettings.json
+            var config = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .Build();
+
+            // Lấy connection string
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            // Tạo options builder
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            // Trả về DbContext với options đã config
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
--- WebAPI/WebView/obj/Debug/net8.0/WebView.AssemblyInfo.cs
+++ WebAPI/WebView/obj/Debug/net8.0/WebView.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebView")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a815870cca05d855609feb50c8e61d250026f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec27006b657807063604b5ee91ae698eeeda97b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebView")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebView")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
